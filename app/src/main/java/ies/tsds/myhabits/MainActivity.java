d02d734617ff8f7fbfed9d91f0173bc58d0b1ea5
Marcio <marcioalico@gmail.com>
2019-10-30 20:18:46 -0300

    Cuarto commit: Se agrega menu de actualizacion  de las tareas agregadas

--- app/src/main/java/ies/tsds/myhabits/MainActivity.java
+++ app/src/main/java/ies/tsds/myhabits/MainActivity.java
@@ -1,15 +1,21 @@
 package ies.tsds.myhabits;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
@@ -90,5 +96,58 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         adapter.stopListening();
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if(item.getTitle().equals("Actualizar")) {
+            mostrarMensajeActz(adapter.getRef(item.getOrder()).getKey(), adapter.getItem(item.getOrder()));// Metodo para mostrar mensaje de actualizacion de tarea
+        }else if (item.getTitle().equals("Eliminar")){
+            eliminarTarea(adapter.getRef(item.getOrder()).getKey());
+
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    private void eliminarTarea(String key) {
+
+
+    }
+
+    private void mostrarMensajeActz(final String key, ToDo item) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Actualizar");
+        builder.setMessage("Actualiza los campos");
+
+        View actualizar_layout =  LayoutInflater.from(this).inflate(R.layout.activity_custom, null);
+
+        final EditText edt_actz_tarea = actualizar_layout.findViewById(R.id.editar_actz_tarea);
+        final EditText edt_actz_dias = actualizar_layout.findViewById(R.id.editar_actz_dias);
+
+
+        edt_actz_tarea.setText(item.getTarea());
+        edt_actz_dias.setText(item.getDias());
+
+        builder.setView(actualizar_layout);
+        builder.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String tarea = edt_actz_tarea.getText().toString();
+                String dias = edt_actz_dias.getText().toString();
+
+                ToDo toDo = new ToDo(tarea, dias);
+                todoDb.child(key).setValue(toDo);
+
+                Toast.makeText(MainActivity.this, "La Tarea  fue Actualizada", Toast.LENGTH_SHORT).show();
+          }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
     }
 }
--- app/src/main/java/ies/tsds/myhabits/vista/VistaToDo.java
+++ app/src/main/java/ies/tsds/myhabits/vista/VistaToDo.java
@@ -2,12 +2,13 @@ package ies.tsds.myhabits.vista;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.ContextMenu;
 import android.view.View;
 import android.widget.TextView;
 
 import ies.tsds.myhabits.R;
 
-public class VistaToDo extends RecyclerView.ViewHolder {
+public class VistaToDo extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener{
 
     public TextView texto_tarea, texto_dias;
 
@@ -16,5 +17,14 @@ public class VistaToDo extends RecyclerView.ViewHolder {
 
         texto_tarea = itemView.findViewById(R.id.texto_tarea);
         texto_dias = itemView.findViewById(R.id.texto_dias);
+        itemView.setOnCreateContextMenuListener(this);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+        menu.setHeaderTitle("Seleccionar Opcion");
+        menu.add(0,0, getAdapterPosition(), "Actualizar");
+        menu.add(0,1, getAdapterPosition(), "Eliminar");
     }
 }
